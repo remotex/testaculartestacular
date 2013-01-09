a4b47ea6f910ab7e532e291f59a6f89e3a98e9c9
Johan Andersson <johan-spam@andersson.net>
2013-01-09 18:27:08 +0100

    Initial commit of start-host.ps1 and queue-run.ps1

---
+++ queue-run.ps1
@@ -0,0 +1,69 @@
+﻿param( 
+	[parameter(mandatory=$true)]
+	$testacularConfigPath, 
+	$workspaceDir,
+	$hostTimeout = (5 * 60)
+)
+
+$scriptdir = $MyInvocation.MyCommand.Path | Split-Path
+if(!$workspaceDir) {
+	$workspaceDir = $scriptdir
+}
+if( !(Test-Path $workspaceDir) ) {
+	throw "Workspace directory specified does not exist: $workspaceDir"
+}
+$workspaceDir = Resolve-Path $workspaceDir
+
+$runId = 1
+$nextRunIdPath = Join-Path $workspaceDir "nextRunNumber"
+if( Test-Path $nextRunIdPath ) {
+	[int]$runId = gc $nextRunIdPath | select -First 1
+}
+($runId + 1) | sc $nextRunIdPath
+
+$runDir = Join-Path $workspaceDir "runs\$runId"
+if( Test-Path $runDir ) {
+	rmdir -Recurse -Force $runDir 
+}
+
+mkdir $runDir | Out-Null
+$runStatusPath = Join-Path $runDir "status"
+
+$watcher = New-Object IO.FileSystemWatcher $runDir, "status" -Property @{IncludeSubdirectories = $false;NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'} 
+$watcherChangedId = "RunStatusWatcherChanged$runId"
+$watcherCreatedId = "RunStatusWatcherCreated$runId"
+Register-ObjectEvent -InputObject $watcher -EventName Changed -SourceIdentifier $watcherChangedId
+Register-ObjectEvent -InputObject $watcher -EventName Created -SourceIdentifier $watcherCreatedId
+$watcher.EnableRaisingEvents = $true
+
+Write-Host "Run $runId is waiting for available executor"
+$testacularConfigPath | sc $runDir\runOptions
+$createdEvent = Wait-Event -SourceIdentifier $watcherCreatedId -Timeout $hostTimeout
+if( $createdEvent ) {
+	$createdEvent | Remove-Event
+} else {
+	Write-Host "Run timed out: Host did not pick up before the timeout of $hostTimeout seconds occurred"
+}
+
+try {
+	while($createdEvent) {
+		$event = Wait-Event -SourceIdentifier $watcherChangedId
+		$event | Remove-Event
+		$status = gc $runStatusPath | select -First 1
+		Write-Host "Run $runId status: $status"
+		if( $status -match "FAILED|SUCCESS" ) {
+			if( Test-Path $runDir\output ) {
+				Write-Host "Output:"
+				gc $runDir\output | %{ "    $_" } | Out-Host
+				Write-Host "--EOF--"
+			} else {
+				Write-Host "No output from run"
+			}
+			break
+		}
+	}
+} finally {
+	Unregister-Event -SourceIdentifier $watcherChangedId
+	Unregister-Event -SourceIdentifier $watcherCreatedId
+	$watcher.Dispose()
+}
---
+++ start-host.ps1
@@ -0,0 +1,100 @@
+﻿param( $testacularDir = $(join-path -Resolve $env:APPDATA "npm\node_modules\testacular"), $port = 8080, $workspaceDir )
+
+$scriptdir = $MyInvocation.MyCommand.Path | Split-Path
+$hostConfigPath = join-path -Resolve $scriptdir "emptyhost.conf.js"
+$outFilename = "testacular.out.log"
+$errFilename = "testacular.err.log"
+$hostOutputPath = join-path $scriptdir $outFilename
+$hostErrorPath = join-path $scriptdir $errFilename
+$nodePath = Get-Command node | select -ExpandProperty Definition
+
+if(!$workspaceDir) {
+	$workspaceDir = $scriptdir
+}
+
+function startTestacular( $configPath, $outputFilePath, $errorFilePath, [switch] $singleRun ) {
+	$command = "$testacularDir\bin\testacular start $configPath --no-colors --port $port"
+	if( $singleRun ) {
+		$command += " --single-run"
+	}
+	Write-Host -NoNewline "Starting testacular: '$command'"
+	$proc = Start-Process -FilePath $nodePath -ArgumentList $command -NoNewWindow -PassThru -RedirectStandardOutput $outputFilePath -RedirectStandardError $errorFilePath
+	Write-Host " (PID $($proc.Id))"
+	$proc
+}
+
+function startPlaceholderHost {
+	Write-Host "Starting new placeholder host"
+	startTestacular $hostConfigPath -outputFilePath $hostOutputPath -errorFilePath $hostErrorPath
+}
+
+function writeRunOutput( $runDir, $message ) {
+	Write-Host $message
+	$message | ac $runDir\output
+}
+
+function executeRun( $runOptionsFile, $hostProcess ) {
+	$runDir = $runOptionsFile | Split-Path
+	$runId = $runDir | Split-Path -Leaf
+	"RUNNING" | sc $runDir\status
+	writeRunOutput $runDir "Run $runId was created at $runDir"
+	$runConfigPath = gc $runOptionsFile | select -First 1
+	if( !(Test-Path $runConfigPath) ) {
+		writeRunOutput $runDir "Cannot find config file '$runConfigPath'"
+		"FAILED" | sc $runDir\status
+		return
+	}
+		
+	writeRunOutput $runDir "Killing default host (PID: $($hostProcess.Id))"
+	$hostProcess.Kill()
+	writeRunOutput $runDir "Waiting for default host to exit (PID: $($hostProcess.Id))"	
+	$hostProcess.WaitForExit()
+	
+	writeRunOutput $runDir "Starting run with config $runConfigPath"
+	$outputFilePath = Join-Path $runDir $outFilename
+	$errorFilePath = Join-Path $runDir $errFilename
+	$runProcess = startTestacular $runConfigPath -outputFilePath $outputFilePath -errorFilePath $errorFilePath -singleRun
+	writeRunOutput $runDir "Waiting for run to complete (PID: $($runProcess.Id))"
+	$status = "FAILED"
+	if( $runProcess.WaitForExit( [TimeSpan]::FromMinutes(10).TotalMilliseconds ) ) {
+		gc $outputFilePath | %{ writeRunOutput $runDir $_ }
+		if( (gi $errorFilePath).Length -eq 0 ) {
+			writeRunOutput $runDir "Run $runId completed successfully"
+			$status = "SUCCESS"
+		} else {
+			writeRunOutput $runDir "Run $runId failed due to errors:"
+			gc $errorFilePath | %{ writeRunOutput $runDir $_ }
+		}
+	} else {
+		writeRunOutput $runDir "Run $runId failed due to timeout"
+	}		
+	$status | sc $runDir\status
+}
+
+$runsDir = join-path $workspaceDir "runs"
+if(!(test-path $runsDir)) {
+	mkdir $runsDir | Out-Null
+}
+$hostProcess = startPlaceholderHost
+
+$watcher = New-Object IO.FileSystemWatcher $runsDir, "runOptions" -Property @{IncludeSubdirectories = $true;NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'} 
+$watcherId = "NewRunWatcher"
+Register-ObjectEvent -InputObject $watcher -EventName Created -SourceIdentifier $watcherId
+$watcher.EnableRaisingEvents = $true
+
+try {
+	while(1) {
+		Write-Host "Waiting for new runs in $runsDir"
+		$event = Wait-Event -SourceIdentifier $watcherId
+		$event | Remove-Event
+		$runDir = $event.SourceEventArgs.FullPath
+		Write-Host "Executing run for $runDir"
+		executeRun $runDir $hostProcess
+		if( $hostProcess.HasExited ) {
+			$hostProcess = startPlaceholderHost
+		}
+	}
+} finally {
+	Unregister-Event -SourceIdentifier $watcherId
+	$watcher.Dispose()
+}
